d0a37e28bd210e73d239e452c49c1a8c3fe09b8b
NguyenThiLinh <linh.nguyenthi@ncc.asia>
2021-06-27 15:17:22 +0700

    init gatsby md

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# SPA vs SSR vs SSG
+
+
+### Reference
+https://blog.logrocket.com/next-js-vs-gatsbyjs-a-developers-perspective/
---
+++ gatsby/blog3/src/pages/blog.js
@@ -0,0 +1,77 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Button from "../components/button"
+
+class Blog extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allMarkdownRemark.edges
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title="All posts" />
+        <div style={{ margin: "20px 0 40px" }}>
+          {posts.map(({ node }) => {
+            const title = node.frontmatter.title || node.fields.slug
+            return (
+              <div key={node.fields.slug}>
+                <h3
+                  style={{
+                    marginBottom: 1,
+                  }}
+                >
+                  <Link
+                    style={{ boxShadow: `none` }}
+                    to={`blog${node.fields.slug}`}
+                  >
+                    {title}
+                  </Link>
+                </h3>
+                <small>{node.frontmatter.date}</small>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: node.frontmatter.description || node.excerpt,
+                  }}
+                />
+              </div>
+            )
+          })}
+        </div>
+        <Link to="/">
+          <Button marginTop="85px">Go Home</Button>
+        </Link>
+      </Layout>
+    )
+  }
+}
+
+export default Blog
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
